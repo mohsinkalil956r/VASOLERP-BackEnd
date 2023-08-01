b5b5bf84b70fc5f2039eaa73d189929e9d2e8adf
Awais744 <razaawais744@gmail.com>
2023-08-01 20:28:06 +0500

    feat: contacts-api-implementation

--- ERP.API/Controllers/ContactsController.cs
+++ ERP.API/Controllers/ContactsController.cs
@@ -24,37 +24,73 @@ namespace ERP.API.Controllers
         }
 
 
-        // GET: api/<ValuesController>
         [HttpGet]
-        public async Task<IActionResult> Get()
+        public async Task<IActionResult> Get(string? searchValue = "", int pageNumber = 1, int pageSize = 10)
         {
             var employeeContacts = await this._employeeContact.Get()
                 .Include(e => e.Employee)
-                .Select(c => new { Type = "Employee",
-                EmployeeId = c.EmployeeId,
-                FirstName = c.Employee.FirstName,
-                LastName = c.Employee.LastName,
+                .Select(e => new
+                {
+                    Type = "Employee",
+                    Id = e.EmployeeId, // Assuming EmployeeId is the unique identifier for employees
+                    FirstName = e.Employee.FirstName,
+                    LastName = e.Employee.LastName,
                 })
                 .ToListAsync();
 
             var clientContacts = await this._clientContact.Get()
-                .Include(e => e.Client)
-                .Select(c => new { Type = "Client",
-                ClientId = c.ClientId,
-                Name = c.Client.Name })
+                .Include(c => c.Client)
+                .Select(c => new
+                {
+                    Type = "Client",
+                    Id = c.ClientId, // Assuming ClientId is the unique identifier for clients
+                    FirstName = c.Client.FirstName,
+                    LastName = c.Client.LastName
+                })
                 .ToListAsync();
 
-            //List<string> concatinated = employeeContacts.Concat(clientContacts).ToList();
+            // Concatenate employeeContacts and clientContacts into a single list
+            var mergeLists = employeeContacts.Concat(clientContacts).ToList();
+
+            // Apply search filter if searchValue is provided and not null or empty
+            if (!string.IsNullOrEmpty(searchValue))
+            {
+                mergeLists = mergeLists.Where(p =>
+                    p.Id.ToString().Contains(searchValue) || // Assuming Id is an integer; convert to string for search
+                    p.FirstName.Contains(searchValue) ||
+                    p.LastName.Contains(searchValue)
+                ).ToList();
+            }
+
+            // Get the total count of items without pagination
+            var totalCount = mergeLists.Count;
+
+            // Apply pagination
+            mergeLists = mergeLists.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
+
+            var result = mergeLists.Select(p => new
+            {
+                p.Id,
+                p.FirstName,
+                p.LastName,
+                p.Type // Include the Type in the result
+            }).ToList();
 
             return Ok(new APIResponse<object>
             {
                 IsError = false,
                 Message = "",
-                data = new List<object>().Concat(employeeContacts).Concat(clientContacts).ToList()
+                data = new
+                {
+                    TotalCount = totalCount,
+                    PageSize = pageSize,
+                    CurrentPage = pageNumber,
+                    SearchValue = searchValue,
+                    Results = result
+                }
+            });
+        }
 
-        }
-                     );
-        }
 
 
     }
