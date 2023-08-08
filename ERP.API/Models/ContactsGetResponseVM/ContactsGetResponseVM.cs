167479d08f007fa16537ac63eff3eda65ba6cfe5
Awais744 <razaawais744@gmail.com>
2023-08-08 17:34:41 +0500

    feat: contacts-pagination-implemented

---
+++ ERP.API/Models/ContactsGetResponseVM/ContactsGetResponseVM.cs
@@ -0,0 +1,12 @@
+﻿namespace ERP.API.Models.ContactsGetResponseVM
+{
+    public class ContactsGetResponseVM
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string DepartmentName { get; set; }
+        public string Type { get; set; }
+    }
+}
