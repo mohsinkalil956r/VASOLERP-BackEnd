92c304544942ca3c2356f0d4f2ee5514d826fdb9
M Sheraz <ranasheraz963@gmail.com>
2023-08-22 13:22:12 +0500

    feat: productversion controller

---
+++ ERP.API/Controllers/ProductVersionController.cs
@@ -0,0 +1,19 @@
+﻿using ERP.DAL.Repositories.Abstraction;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ERP.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductVersionController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetVersion()
+        {
+            string version = "1.0.0"; // Hardcoded version
+
+            return Ok(new { Version = version });
+        }
+    }
+}
